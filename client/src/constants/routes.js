108f4a3ea3d775c0c78f6d79938f869d88bc9ac7
hung.ho <wacui113@gmail.com>
2022-05-22 10:40:12 +0700

    +Add login form +add forgotpwd form +update more _button css -remove body div tag css in Auth/index.js +add _login page css +add route for forgotpwd index.js

--- client/src/constants/routes.js
+++ client/src/constants/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Dashboard from '../views/Dashboard';
 import Login from '../views/Login';
+import ForgotPassword from '../views/ForgotPassword';
 
 export const routeMain = [
     { path: '/dashboard', main: (props) => <Dashboard {...props} /> },
@@ -9,4 +10,5 @@ export const routeMain = [
 
 export const routeAuth = [
     { path: '/auth/login', main: (props) => <Login {...props} /> },
+    { path: '/auth/forgotPassword', main: (props) => <ForgotPassword {...props} /> },
 ]
